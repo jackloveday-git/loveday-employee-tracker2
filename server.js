f6cda74b8648a32a508bb03304a60e418e58f88e
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-11-03 19:26:30 -0400

    Created schmea, server edits

--- server.js
+++ server.js
@@ -95,13 +95,15 @@ function employeeDepartment() {
 
     // Enter SQL Query
     var query =
-        `SELECT d.id, d.name, r.salary AS budget
-  FROM employee e
-  LEFT JOIN role r
-	ON e.role_id = r.id
-  LEFT JOIN department d
-  ON d.id = r.department_id
-  GROUP BY d.id, d.name`
+        `
+        SELECT d.id, d.name, r.salary AS budget
+        FROM employee e
+        LEFT JOIN role r
+	    ON e.role_id = r.id
+        LEFT JOIN department d
+        ON d.id = r.department_id
+        GROUP BY d.id, d.name
+        `
 
     // Send Query to our SQL
     connection.query(query, (err, res) => {
@@ -121,14 +123,16 @@ function allEmployees() {
 
     // Enter SQL Query
     var query =
-        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
-  FROM employee e
-  LEFT JOIN role r
-	ON e.role_id = r.id
-  LEFT JOIN department d
-  ON d.id = r.department_id
-  LEFT JOIN employee m
-	ON m.id = e.manager_id`
+        `
+        SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+        FROM employee e
+        LEFT JOIN role r
+	    ON e.role_id = r.id
+        LEFT JOIN department d
+        ON d.id = r.department_id
+        LEFT JOIN employee m
+	    ON m.id = e.manager_id
+        `
 
     // Send Query to our SQL
     connection.query(query, function (err, res) {
@@ -146,8 +150,10 @@ function addEmployee() {
 
     // Enter SQL Query
     var query =
-        `SELECT r.id, r.title, r.salary 
-      FROM role r`
+        `
+        SELECT r.id, r.title, r.salary 
+        FROM role r
+        `
 
     // Send Query to our SQL
     connection.query(query, function (err, res) {
@@ -175,13 +181,16 @@ function departmentInput(depts) {
         .then((answer) => {
             console.log("answer ", answer.departmentId);
             var query =
-                `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department 
-  FROM employee e
-  JOIN role r
-	ON e.role_id = r.id
-  JOIN department d
-  ON d.id = r.department_id
-  WHERE d.id = ?`
+                `
+                SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department 
+                FROM employee e
+                JOIN role r
+	            ON e.role_id = r.id
+                JOIN department d
+                ON d.id = r.department_id
+                WHERE d.id = ?
+                `
+
             connection.query(query, answer.departmentId, function (err, res) {
                 if (err) throw err;
                 console.table("response ", res);
@@ -287,14 +296,16 @@ function employeeList() {
     console.log("Updating an employee");
 
     var query =
-        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
-  FROM employee e
-  JOIN role r
-	ON e.role_id = r.id
-  JOIN department d
-  ON d.id = r.department_id
-  JOIN employee m
-	ON m.id = e.manager_id`
+        `
+        SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+        FROM employee e
+        JOIN role r
+	    ON e.role_id = r.id
+        JOIN department d
+        ON d.id = r.department_id
+        JOIN employee m
+	    ON m.id = e.manager_id
+        `
 
     connection.query(query, function (err, res) {
         if (err) throw err;
@@ -311,8 +322,11 @@ function employeeList() {
 function roleArray(employeeChoices) {
     console.log("Updating an role");
     var query =
-        `SELECT r.id, r.title, r.salary 
-  FROM role r`
+        `
+        SELECT r.id, r.title, r.salary 
+        FROM role r
+        `
+
     let roleInput;
     connection.query(query, function (err, res) {
         if (err) throw err;
@@ -400,13 +414,15 @@ function promptAddRole(depts) {
 // Add a Role
 function addRole() {
     var query =
-        `SELECT d.id, d.name, r.salary AS budget
-    FROM employee e
-    JOIN role r
-    ON e.role_id = r.id
-    JOIN department d
-    ON d.id = r.department_id
-    GROUP BY d.id, d.name`
+        `
+        SELECT d.id, d.name, r.salary AS budget
+        FROM employee e
+        JOIN role r
+        ON e.role_id = r.id
+        JOIN department d
+        ON d.id = r.department_id
+        GROUP BY d.id, d.name
+        `
 
     connection.query(query, function (err, res) {
         if (err) throw err;
